9d724ef9fd876fea968e31f73aac058037cb9125
jiajun.li <jiajun.li@rea-group.com>
2022-11-15 23:16:01 +0800

    add tests for Team

---
+++ src/test/java/com/tg/team/TeamTest.java
@@ -0,0 +1,40 @@
+package com.tg.team;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class TeamTest {
+
+    @Test
+    void shouldAssignBASuccessful_givenOneBA_whenAssignMember() {
+        Team team = new Team();
+        Member member = new BA();
+
+        assertTrue(team.assignMember(member));
+    }
+
+    @Test
+    void shouldAssignDevSuccessful_givenOneBA_whenAssignMember() {
+        Team team = new Team();
+        Member member = new Dev();
+
+        assertTrue(team.assignMember(member));
+    }
+
+    @Test
+    void shouldAssignQASuccessful_givenOneBA_whenAssignMember() {
+        Team team = new Team();
+        Member member = new QA();
+
+        assertTrue(team.assignMember(member));
+    }
+
+    @Test
+    void shouldAssignStorySuccessful_givenOneStory_whenAssignStory() {
+        Team team = new Team();
+        Story story = new Story();
+
+        assertTrue(team.assignStory(story));
+    }
+}
